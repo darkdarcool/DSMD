653c38711e7983d01b0de5799b5db7e104fc21bb
darkdarcool <darrendreddy@gmail.com>
2021-07-26 12:18:27 -0400

    FS

--- entry.js
+++ entry.js
@@ -1,9 +1,7 @@
 var DSMD = require('./src/index.js');
-var fs = require('fs')
 
 module.exports = {
   toMarkdown: DSMD.DSMD_Markdown,
   toHTML: DSMD.DSMD_HTML
 };
 
-fs.writeFileSync("./readme.md", DSMD.DSMD_Markdown(fs.readFileSync('./readme.dsmd', encoding = "utf-8")))
